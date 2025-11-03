d9d2af24afc52178e90fbe4b4367aa096d502b89
ricoberger <mail@ricoberger.de>
2025-11-03 20:02:54 +0100

    Neovim: Update `snacks.nvim` and Improve Notifications

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -536,6 +536,10 @@ require("snacks").setup({
   input = {
     enabled = true,
   },
+  -- Pretty vim.notify
+  notifier = {
+    enabled = true,
+  },
   -- Picker for selecting items such as files, grep results, buffers, marks,
   -- etc.
   picker = {
@@ -1144,6 +1148,22 @@ vim.api.nvim_create_autocmd("LspAttach", {
         })
       end
     end
+  end,
+})
+
+-- Show LSP progress in a notification.
+vim.api.nvim_create_autocmd("LspProgress", {
+  callback = function(event)
+    local spinner =
+      { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+    vim.notify(vim.lsp.status(), "info", {
+      id = "lsp_progress",
+      title = "LSP Progress",
+      opts = function(notif)
+        notif.icon = event.data.params.value.kind == "end" and " "
+          or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+      end,
+    })
   end,
 })
 
