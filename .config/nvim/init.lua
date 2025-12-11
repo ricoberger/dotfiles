e1a586709c1c6aa96afa355a3daa6de39aaa1a33
ricoberger <mail@ricoberger.de>
2025-12-11 20:11:27 +0100

    Neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,25 +7,29 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Disable some built-in plugins, so that they are not loaded.
-vim.g.loaded_gzip = 1
-vim.g.loaded_tar = 1
-vim.g.loaded_tarPlugin = 1
-vim.g.loaded_zip = 1
-vim.g.loaded_zipPlugin = 1
+vim.g.loaded_2html_plugin = 1
 vim.g.loaded_getscript = 1
 vim.g.loaded_getscriptPlugin = 1
-vim.g.loaded_vimball = 1
-vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_logiPat = 1
 vim.g.loaded_matchit = 1
 vim.g.loaded_matchparen = 1
-vim.g.loaded_2html_plugin = 1
-vim.g.loaded_logiPat = 1
-vim.g.loaded_rrhelper = 1
 vim.g.loaded_netrw = 1
+vim.g.loaded_netrw_gitignore = 1
+vim.g.loaded_netrwFileHandlers = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrwSettings = 1
-vim.g.loaded_netrwFileHandlers = 1
-vim.g.loaded_netrw_gitignore = 1
+vim.g.loaded_remote_plugins = 1
+vim.g.loaded_rplugin = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_tohtml = 1
+vim.g.loaded_tutor = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
 
 -- We are using "Cascadia Code" as font in our terminal, so that we can enable
 -- nerd font support in Neovim.
